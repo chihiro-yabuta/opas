68782dc9577cdd5c36b310a8467d6272590006f4
chihiro-yabuta <ja.chihiro.yabuta@gmail.com>
2024-09-30 12:55:22 +0000

    add region

--- pages/genre.tsx
+++ pages/genre.tsx
@@ -7,49 +7,43 @@ export default function Genre() {
   const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
-    document.addEventListener('click', (e) => {
-      if (ref.current && !ref.current.contains(e.target)) {
-        setIsVisible(false);
-      }
-    });
+    document.addEventListener('click', e => !ref.current.contains(e.target) && setIsVisible(false));
   }, []);
 
-  return (
-    <>
-      <motion.div
-        ref={ref}
-        style={{
-          textAlign: 'center', border: '2px solid #043ab9', borderRadius: '30px',
-          fontSize: '2vw', padding: '10px 0',
-        }}
-        onClick={() => setIsVisible(true)}
-        children={genre}
-      />
-      <motion.div
-        initial={{ height: 0, opacity: 0 }}
-        animate={{ height: isVisible ? 'auto' : 0, opacity: Number(isVisible) }}
-        style={{ overflow: 'hidden' }}
+  return <>
+    <motion.div
+      ref={ref}
+      style={{
+        textAlign: 'center', border: '2px solid #043ab9', borderRadius: '30px',
+        fontSize: '2vw', padding: '10px 0',
+      }}
+      onClick={() => setIsVisible(true)}
+      children={genre}
+    />
+    <motion.div
+      initial={{ height: 0, opacity: 0 }}
+      animate={{ height: isVisible ? 'auto' : 0, opacity: Number(isVisible) }}
+      style={{ overflow: 'hidden' }}
+    >
+      {genres.map((gl, i) => <div
+        key={i}
+        style={{ display: 'flex', justifyContent: 'center' }}
       >
-        {genres.map((gl, i) => <div
-          key={i}
-          style={{ display: 'flex', justifyContent: 'center' }}
-        >
-          {...gl.map((g, n) => <motion.div
-            key={i * 3 + n}
-            initial={{ backgroundColor: '#ffffff', border: '2px solid #84a2d4' }}
-            whileHover={{ backgroundColor: '#e0ffff' }}
-            animate={{ border: `2px solid ${genre === g ? '#043ab9' : '#84a2d4'}` }}
-            style={{
-              width: '25%', textAlign: 'center', borderRadius: '30px',
-              fontSize: '1.5vw', margin: '10px 1%', padding: '10px 0',
-            }}
-            onClick={() => setGenre(g)}
-            children={g}
-          />)}
-        </div>)}
-      </motion.div>
-    </>
-  )
+        {...gl.map((g, n) => <motion.div
+          key={i * 3 + n}
+          initial={{ backgroundColor: '#ffffff', border: '2px solid #84a2d4' }}
+          whileHover={{ backgroundColor: '#e0ffff' }}
+          animate={{ border: `2px solid ${genre === g ? '#043ab9' : '#84a2d4'}` }}
+          style={{
+            width: '25%', textAlign: 'center', borderRadius: '30px',
+            fontSize: '1.5vw', margin: '10px 1%', padding: '10px 0',
+          }}
+          onClick={() => setGenre(g)}
+          children={g}
+        />)}
+      </div>)}
+    </motion.div>
+  </>
 }
 
 const genres = [
--- pages/region.tsx
+++ pages/region.tsx
@@ -1,7 +1,44 @@
+import React, { useState } from 'react';
+import { motion } from 'framer-motion';
+
 export default function Region() {
-  return (
-    <>
-      preparing
-    </>
-  )
+  const [region, setRegion] = useState(regions[0]);
+
+  return <div
+    style={{ display: 'flex', justifyContent: 'center' }}
+  >
+    {regions.map((r, i) => <motion.div
+      key={i}
+      initial={{ backgroundColor: '#ffffff' }}
+      whileHover={{ backgroundColor: `${region === r ? '#84a2d4' : '#e0ffff'}` }}
+      animate={{ backgroundColor: `${region === r ? '#84a2d4' : '#ffffff'}` }}
+      style={{
+        display: 'grid', writingMode: 'vertical-rl', placeItems: 'center',
+        boxShadow: '0 0 0 2px #84a2d4', width: '5%', fontSize: '2vw', padding: '20px 0',
+      }}
+      onClick={() => setRegion(r)}
+      children={r}
+    />)}
+  </div>
 }
+
+const regions = [
+  '大阪府',
+  '豊中市',
+  '茨木市',
+  '堺市',
+  '高石市',
+  '大阪狭山市',
+  '熊取町',
+  '池田市',
+  '東大阪市',
+  '吹田市',
+  '和泉市',
+  '高槻市',
+  '河内長野市',
+  '大阪市',
+  '富田林市',
+  '河南町',
+  '太子町',
+  '千早赤阪村',
+];
