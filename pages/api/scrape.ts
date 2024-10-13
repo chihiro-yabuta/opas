b8a05796b1c4888fb5189380412b7e5ea0e0c858
chihiro-yabuta <ja.chihiro.yabuta@gmail.com>
2024-10-13 13:15:49 +0000

    rm

--- pages/api/scrape.ts
+++ pages/api/scrape.ts
@@ -1,6 +1,6 @@
 import { appendFile } from 'fs/promises';
 import { RedisClientType } from 'redis';
-import { Page, Browser, ElementHandle, launch } from 'puppeteer';
+import { Page, Browser, ElementHandle, launch } from 'puppeteer-core';
 
 export async function scrape(cli: RedisClientType, reqRegion: string, reqGenre: string) {
   let browserInit: Browser;
@@ -184,8 +184,10 @@ interface Log {
 }
 
 const args = {
+  executablePath: '/usr/bin/chromium',
   args: [
     '--no-sandbox',
     '--disable-setuid-sandbox',
   ],
+  timeout: 0,
 }
